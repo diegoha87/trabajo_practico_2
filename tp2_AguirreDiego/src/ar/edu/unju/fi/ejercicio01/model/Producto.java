b9a779c5300b46d28419ad6e008318b7b8f865ad
diego <diego@DiegoHA>
2024-04-26 00:16:43 -0300

    Ejercicio 1 resuelto - Aguirre Diego

--- tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
+++ tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio01/model/Producto.java
@@ -1,5 +1,93 @@
 package ar.edu.unju.fi.ejercicio01.model;
 
 public class Producto {
+	private String codigo;
+	private String descripcion;
+	private double precioUnitario;
+	private origenFabricacion origen;
+	private Categoria categoria;
+	
+	public Producto() {
+		
+	}
+	
+	
+	
+	public Producto(String codigo, String descripcion, double precioUnitario, origenFabricacion origen, Categoria categoria) {
+		this.codigo = codigo;
+		this.descripcion = descripcion;
+		this.precioUnitario = precioUnitario;
+		this.origen = origen;
+		this.categoria = categoria;
+	}
 
+
+
+	public enum origenFabricacion{
+		ARGENTINA, CHINA, BRASIL, URUGUAY
+	}
+	
+	public enum Categoria{
+		TELEFONIA, INFORMATICA, ELECTROHOGAR, HERRAMIENTAS
+	}
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+
+
+	public void setCodigo(String codigo) {
+		this.codigo = codigo;
+	}
+
+
+
+	public String getDescripcion() {
+		return descripcion;
+	}
+
+
+
+	public void setDescripcion(String descripcion) {
+		this.descripcion = descripcion;
+	}
+
+
+
+	public double getPrecioUnitario() {
+		return precioUnitario;
+	}
+
+
+
+	public void setPrecioUnitario(double precioUnitario) {
+		this.precioUnitario = precioUnitario;
+	}
+
+
+
+	public origenFabricacion getOrigen() {
+		return origen;
+	}
+
+
+
+	public void setOrigen(origenFabricacion origen) {
+		this.origen = origen;
+	}
+
+
+
+	public Categoria getCategoria() {
+		return categoria;
+	}
+
+
+
+	public void setCategoria(Categoria categoria) {
+		this.categoria = categoria;
+	}
+	
+	
 }
