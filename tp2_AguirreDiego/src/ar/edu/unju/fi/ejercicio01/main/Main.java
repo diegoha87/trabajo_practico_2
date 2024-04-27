1fcbaf891dbba12db4c4cce91c219e1202bf6c70
diego <diego@DiegoHA>
2024-04-27 15:26:19 -0300

    Correcion de la opción "modificar prodcuto" del menu

--- tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio01/main/Main.java
+++ tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio01/main/Main.java
@@ -177,127 +177,127 @@ public class Main {
 					System.out.println("---------------------------------");
 				}
 				else {
+					boolean band = false;
+					System.out.println("-------Modificar producto--------- ");
+					System.out.println("Ingrese el codigo del producto:");
+					codigo = scanner.next();
+					for(int i= 0; i < listaProductos.size() && band == false; i++) {
+						Producto elementoLista = listaProductos.get(i);
+						if (elementoLista.getCodigo().equals(codigo)) {
+							band = true;
+							scanner.nextLine();
+							System.out.println("Ingrese la descripción del producto: ");
+							descripcion = scanner.nextLine();
+							do {
+								try {
+									System.out.println("Ingrese el precio del producto: ");
+									precioUnitario = scanner.nextDouble();
+									repetir2 = false;
+								}
+								catch(InputMismatchException e) {
+									scanner.nextLine();
+									System.out.println("--------------------------------------------");
+									System.out.println("Error: el precio debe ser un valor númerico.");
+									System.out.println("--------------------------------------------");
+								}
+							}while(repetir2);
+							
+							do {
+								try {
+									System.out.println("----Origen de fabricación-----");
+									System.out.println("1- Argentina.");
+									System.out.println("2- China.");
+									System.out.println("3-Brasil.");
+									System.out.println("4-Uruguay.");
+									System.out.println(" ");
+									System.out.println("Elija una opción: ");
+									opcionOrigen = scanner.nextInt();
+									repetirOrigen = false;
+									
+									switch(opcionOrigen) {
+									case 1:
+										origen = origenFabricacion.ARGENTINA;
+										break;
+									case 2:
+										origen = origenFabricacion.CHINA;
+										break;
+									case 3:
+										origen = origenFabricacion.BRASIL;
+										break;
+									case 4:
+										origen = origenFabricacion.URUGUAY;
+										break;
+									default:
+										repetirOrigen = true;
+										System.out.println("----------------------------");
+										System.out.println("Error: Elija un opción correcta");
+										System.out.println("----------------------------");
+									}
+								}
+								catch(InputMismatchException e) {
+									scanner.nextLine();
+									System.out.println("---------------------------------");
+									System.out.println("Error: Elija una opcion correcta.");
+									System.out.println("---------------------------------");
+								}
+							}while(repetirOrigen);
+							
+							do {
+								try {
+									System.out.println("----Categoría-----");
+									System.out.println("1- Telefonía.");
+									System.out.println("2- Informática.");
+									System.out.println("3-Electro hogar.");
+									System.out.println("4-Herramientas.");
+									System.out.println(" ");
+									System.out.println("Elija una opción: ");
+									opcionCategoria = scanner.nextInt();
+									repetirCategoria = false;
+									
+									switch(opcionCategoria) {
+									case 1:
+										categoria = Categoria.TELEFONIA;
+										break;
+									case 2:
+										categoria = Categoria.INFORMATICA;
+										break;
+									case 3:
+										categoria = Categoria.ELECTROHOGAR;
+										break;
+									case 4:
+										categoria = Categoria.HERRAMIENTAS;
+										break;
+									default:
+										repetirCategoria = true;
+										System.out.println("-------------------------------");
+										System.out.println("Error: Elija un opción correcta");
+										System.out.println("-------------------------------");
+									}
+									Producto producto2 = new Producto(codigo, descripcion, precioUnitario, origen, categoria);
+									listaProductos.set(i, producto2);
+								}
+								catch(InputMismatchException e) {
+									scanner.nextLine();
+									System.out.println("---------------------------------");
+									System.out.println("Error: Elija una opcion correcta.");
+									System.out.println("---------------------------------");
+								}
+							}while(repetirCategoria);
+						}
+					}
 					
-				}
-				boolean band = false;
-				System.out.println("-------Modificar producto--------- ");
-				System.out.println("Ingrese el codigo del producto:");
-				codigo = scanner.next();
-				for(int i= 0; i < listaProductos.size() && band == false; i++) {
-					Producto elementoLista = listaProductos.get(i);
-					if (elementoLista.getCodigo().equals(codigo)) {
-						band = true;
-						scanner.nextLine();
-						System.out.println("Ingrese la descripción del producto: ");
-						descripcion = scanner.nextLine();
-						do {
-							try {
-								System.out.println("Ingrese el precio del producto: ");
-								precioUnitario = scanner.nextDouble();
-								repetir2 = false;
-							}
-							catch(InputMismatchException e) {
-								scanner.nextLine();
-								System.out.println("--------------------------------------------");
-								System.out.println("Error: el precio debe ser un valor númerico.");
-								System.out.println("--------------------------------------------");
-							}
-						}while(repetir2);
-						
-						do {
-							try {
-								System.out.println("----Origen de fabricación-----");
-								System.out.println("1- Argentina.");
-								System.out.println("2- China.");
-								System.out.println("3-Brasil.");
-								System.out.println("4-Uruguay.");
-								System.out.println(" ");
-								System.out.println("Elija una opción: ");
-								opcionOrigen = scanner.nextInt();
-								repetirOrigen = false;
-								
-								switch(opcionOrigen) {
-								case 1:
-									origen = origenFabricacion.ARGENTINA;
-									break;
-								case 2:
-									origen = origenFabricacion.CHINA;
-									break;
-								case 3:
-									origen = origenFabricacion.BRASIL;
-									break;
-								case 4:
-									origen = origenFabricacion.URUGUAY;
-									break;
-								default:
-									repetirOrigen = true;
-									System.out.println("----------------------------");
-									System.out.println("Error: Elija un opción correcta");
-									System.out.println("----------------------------");
-								}
-							}
-							catch(InputMismatchException e) {
-								scanner.nextLine();
-								System.out.println("---------------------------------");
-								System.out.println("Error: Elija una opcion correcta.");
-								System.out.println("---------------------------------");
-							}
-						}while(repetirOrigen);
-						
-						do {
-							try {
-								System.out.println("----Categoría-----");
-								System.out.println("1- Telefonía.");
-								System.out.println("2- Informática.");
-								System.out.println("3-Electro hogar.");
-								System.out.println("4-Herramientas.");
-								System.out.println(" ");
-								System.out.println("Elija una opción: ");
-								opcionCategoria = scanner.nextInt();
-								repetirCategoria = false;
-								
-								switch(opcionCategoria) {
-								case 1:
-									categoria = Categoria.TELEFONIA;
-									break;
-								case 2:
-									categoria = Categoria.INFORMATICA;
-									break;
-								case 3:
-									categoria = Categoria.ELECTROHOGAR;
-									break;
-								case 4:
-									categoria = Categoria.HERRAMIENTAS;
-									break;
-								default:
-									repetirCategoria = true;
-									System.out.println("-------------------------------");
-									System.out.println("Error: Elija un opción correcta");
-									System.out.println("-------------------------------");
-								}
-								Producto producto2 = new Producto(codigo, descripcion, precioUnitario, origen, categoria);
-								listaProductos.set(i, producto2);
-							}
-							catch(InputMismatchException e) {
-								scanner.nextLine();
-								System.out.println("---------------------------------");
-								System.out.println("Error: Elija una opcion correcta.");
-								System.out.println("---------------------------------");
-							}
-						}while(repetirCategoria);
+					if (band == false) {
+						System.out.println("---------------------------");
+						System.out.println("El producto no existe.");
+						System.out.println("---------------------------");
+					}
+					else {
+						System.out.println("-----------------------------");
+						System.out.println("El producto ha sido modificado.");
+						System.out.println("-----------------------------");
 					}
 				}
 				
-				if (band == false) {
-					System.out.println("---------------------------");
-					System.out.println("El producto no existe.");
-					System.out.println("---------------------------");
-				}
-				else {
-					System.out.println("-----------------------------");
-					System.out.println("El producto ha sido modificado.");
-					System.out.println("-----------------------------");
-				}
 				break;
 			case 4:
 				System.out.println("Fin del programa");
