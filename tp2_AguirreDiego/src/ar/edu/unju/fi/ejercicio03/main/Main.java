b0618951723a8fc31a32490ec07681c75efc09c5
diego <diego@DiegoHA>
2024-04-27 16:47:45 -0300

    Ejercicio 3 resuelto - Aguirre Diego

--- tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio03/main/Main.java
+++ tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio03/main/Main.java
@@ -1,9 +1,24 @@
 package ar.edu.unju.fi.ejercicio03.main;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import ar.edu.unju.fi.ejercicio03.constantes.Provincia;
+
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		//List<Provincia> listaProvincia = new ArrayList<Provincia>();
+		Provincia [] listaProvincia = Provincia.values();
+		System.out.println("********************PROVINCIAS************************");
+		for(Provincia p: listaProvincia) {
+			System.out.println("-------------------"+p.name()+"-----------------");
+			System.out.println("------------------------------------------------");
+			System.out.println("Cantidad de Población: "+p.getCantidadPoblacion());
+			System.out.println("Superficie: "+p.getSuperficie());
+			System.out.println("Densidad: "+p.calcularDensidadPoblacional());
+			System.out.println("------------------------------------------------");
+		}
 
 	}
 
