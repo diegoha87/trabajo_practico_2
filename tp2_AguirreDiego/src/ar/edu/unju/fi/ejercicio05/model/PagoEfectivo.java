8b7102e672d1b1224fe5a56c4d8169ebf275122e
diego <diego@DiegoHA>
2024-05-04 18:05:39 -0300

    Ejercicio 5 resuelto - Aguirre Diego

--- tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio05/model/PagoEfectivo.java
+++ tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio05/model/PagoEfectivo.java
@@ -1,5 +1,48 @@
 package ar.edu.unju.fi.ejercicio05.model;
 
-public class PagoEfectivo {
+import java.time.LocalDate;
 
+import ar.edu.unju.fi.ejercicio05.interfaces.Pago;
+
+public class PagoEfectivo implements Pago{
+	private  double montoPagado;
+	private LocalDate fechaPago;
+	
+	public PagoEfectivo() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PagoEfectivo(double montoPagado, LocalDate fechaPago) {
+		this.montoPagado = montoPagado;
+		this.fechaPago = fechaPago;
+	}
+
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+	
+	@Override
+	public void realizarPagos(double monto) {
+		this.montoPagado = this.montoPagado - monto * 0.10;
+		
+	}
+	
+	public void imprimirRecibo() {
+		System.out.println("_____________Recibo_______________");
+		System.out.println("Fecha de Pago: "+this.fechaPago);
+		System.out.println("Monto Pagado: "+this.montoPagado);
+		System.out.println("__________________________________");
+	}
 }
--- tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio05/model/PagoTarjeta.java
+++ tp2_AguirreDiego/src/ar/edu/unju/fi/ejercicio05/model/PagoTarjeta.java
@@ -1,5 +1,59 @@
 package ar.edu.unju.fi.ejercicio05.model;
 
-public class PagoTarjeta {
+import java.time.LocalDate;
 
+import ar.edu.unju.fi.ejercicio05.interfaces.Pago;
+
+public class PagoTarjeta implements Pago {
+	private String numeroTarjeta;
+	private LocalDate fechaPago;
+	private double montoPagado;
+	
+	public PagoTarjeta() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public PagoTarjeta(String numeroTarjeta, LocalDate fechaPago, double montoPagado) {
+		this.numeroTarjeta = numeroTarjeta;
+		this.fechaPago = fechaPago;
+		this.montoPagado = montoPagado;
+	}
+
+	public String getNumeroTarjeta() {
+		return numeroTarjeta;
+	}
+
+	public void setNumeroTarjeta(String numeroTarjeta) {
+		this.numeroTarjeta = numeroTarjeta;
+	}
+
+	public LocalDate getFechaPago() {
+		return fechaPago;
+	}
+
+	public void setFechaPago(LocalDate fechaPago) {
+		this.fechaPago = fechaPago;
+	}
+
+	public double getMontoPagado() {
+		return montoPagado;
+	}
+
+	public void setMontoPagado(double montoPagado) {
+		this.montoPagado = montoPagado;
+	}
+	
+	@Override
+	public void realizarPagos(double monto) {
+		this.montoPagado = this.montoPagado + monto * 0.15;
+		
+	}
+	
+	public void imprimirRecibo() {
+		System.out.println("___________________Recibo__________________");
+		System.out.println("Número de la tarjeta: "+this.numeroTarjeta);
+		System.out.println("Fecha de Pago: "+this.fechaPago);
+		System.out.println("Monto Pagado: "+this.montoPagado);
+		System.out.println("___________________________________________");
+	}
 }
